140456f38083e74678cbf3bca668d1b61c57484d
TheManStitch <94526792+TheManStitch@users.noreply.github.com>
2022-06-27 12:53:47 +0200

    check foutmeldingen en comment Calander regel 9

--- src/Model/Abstraction/AbstractGebruiker.java
+++ src/Model/Abstraction/AbstractGebruiker.java
@@ -20,17 +20,6 @@ public abstract class AbstractGebruiker {
         return email;
     }
 
-    //public Gebruiker(String naam){
-        //this.naam = naam;
-
-       // Gebruiker Gebruiker4 = new Gebruiker("Karel L The Genius");
-       // Gebruiker Gebruiker3 = new Gebruiker("Adam 3ezi");
-       // Gebruiker Gebruiker2 = new Gebruiker("Emre Liqui Soldaat");
-       // Gebruiker Gebruiker1 = new Gebruiker("Siddhart Parbo");
-       // lijstGebruikers.add(Gebruiker4);
-       // lijstGebruikers.add(Gebruiker3);
-       // lijstGebruikers.add(Gebruiker2);
-        //lijstGebruikers.add(Gebruiker1);
 
 
 
--- src/Model/Organisatie.java
+++ src/Model/Organisatie.java
@@ -15,5 +15,18 @@ public class Organisatie {
     public ArrayList<Gebruiker> GetAlleGebruikers(){
         return AlleGebruikers;
     }
-}
+    //public Gebruiker(String naam){
+        //this.naam = naam;
+
+        // Gebruiker Gebruiker4 = new Gebruiker("Karel L The Genius");
+        // Gebruiker Gebruiker3 = new Gebruiker("Adam 3ezi");
+        // Gebruiker Gebruiker2 = new Gebruiker("Emre Liqui Soldaat");
+        // Gebruiker Gebruiker1 = new Gebruiker("Siddhart Parbo");
+        // lijstGebruikers.add(Gebruiker4);
+        // lijstGebruikers.add(Gebruiker3);
+        // lijstGebruikers.add(Gebruiker2);
+        //lijstGebruikers.add(Gebruiker1);
+
+    }
+
 
